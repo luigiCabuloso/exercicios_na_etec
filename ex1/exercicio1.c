4618d2fb6b887e457266d56067b83f7edcd00124
albino luigi <albino.rodrigues@aluno.cps.sp.gov.br>
2026-05-23 11:10:49 -0300

    nao aguento mais

---
+++ ex1/exercicio1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main(){
+	/*
+	 * calcular o dobro de um numero
+	 */
+	int valor, resultado, opcao;
+while(opcao =1;(
+	printf (n\"boa noite jovem"/n);
+	printf("insira um valor meu caro leitor.");
+	scanf("%d", &valor);
+	resultado - numero*2;
+	printf ("\nresultado:%d\n",resultado);
+	if(resultado >100) {
+	printf("resultado menor que 100\n");
+	}
+		else{
+			if(resultado == 100);{
+			printf("resultado igual a 100\n"};
+		}else { 
+			printf("resultado menor do que 100\n")
+			}
+			}
+printf("deseja continuar? [1-sim, / 2-não]")
+scanf("%d",&opcao);
+
