de97d1b61ebeea1fc8eceaa3f072113fd5926ae6
luigi_rodrigues <113311447+luigiCabuloso@users.noreply.github.com>
2026-08-24 22:23:50 -0300

    Add fruit array and student data to array.php

--- array.php
+++ array.php
@@ -1,1 +1,16 @@
+<?php
+
+$numero3=array();
+$frutas=array("laranja", "maça", "banana");
+for($i=0; $i<count($frutas); $i++){
+    echo " ".$frutas[$i];
+}
+$lista[]=1;
+$lista[]=2;
+$lista[]="laranja";
+$lista[]="maça";
+$lista[]=["aluno"]["nome"]="joão";
+$lista[]=["aluno"]["idade"]="20";
+$lista[]=["aluno"]["nota"]="7";
+$lista[]=["aluno"]["nota2"]="8.5";
 
